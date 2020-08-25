feec1ea732529150b995850b174250c9e7a32f2d
iliakrock <iliakrock@gmail.com>
2020-08-25 13:34:55 +0300

    update

--- function.php
+++ function.php
@@ -4,7 +4,7 @@ function wordFilter($rows)
 {
     $filtered_text = $rows;
     $filtered_text = str_replace('lips', '****', $filtered_text);
-    $filtered_text = str_replace('echo', '****', $filtered_text);
+    $filtered_text = str_replace('stick', '****', $filtered_text);
     $filtered_text = str_replace('text', '****', $filtered_text);
     $filtered_text = str_replace('echo', '****', $filtered_text);
     return $filtered_text;
--- read.php
+++ read.php
@@ -1,9 +1,17 @@
 <?php
 
+include_once ('function.php');
 
 $comment = $_POST['comment'];
 
 $rows = explode(' ', $comment);
+
+foreach ($rows as $key => $value) {
+    $filtered_text = wordFilter($rows);
+    foreach ($value = $filtered_text) {
+
+    }
+}
 
 
 print_r($rows);
